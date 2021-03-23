545e40928995215a2dfab62098031c3fc0114266
Thiago Reschützegger <thiagoreschutzegger@gmail.com>
2021-03-23 13:35:13 -0300

    update

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from utils import *
 from bs4 import BeautifulSoup
 import os
 
-trilhas = ['Pensamento', 'Matemática']
+trilhas = ['Pensamento', 'Mat']
 
 dbname = 'codeqatualizado'
-db = mysql.connector.connect(host="localhost", user="root", password="")
+db = mysql.connector.connect(host="localhost", user="root", password="", buffered = True)
 
-cursor = db.cursor()
+cursor = db.cursor(buffered=True)
 
 # Recreate database
 cursor.execute("DROP DATABASE " + dbname)
 cursor.execute("CREATE DATABASE " + dbname)
 
-db = mysql.connector.connect(host="localhost", user="root", password="", database=dbname)
-cursor = db.cursor()
+db = mysql.connector.connect(host="localhost", user="root", password="", database=dbname, buffered = True)
+cursor = db.cursor(buffered = True)
 
 # Import existent database
 executeScriptsFromFile('codeqmodelo.sql', cursor)
@@ -24,25 +24,20 @@ print('Importado com sucesso!')
 # Identificando pasta principal
 pasta_trilhas = getSubfolderNameThatContains('', 'Trilhas')
 
-# Identificando pastas das trilhas
-pastas = []
 for trilha in trilhas:
-    pastas.append(pasta_trilhas + '/' + getSubfolderNameThatContains(pasta_trilhas, trilha))
-
-html_trilhas = [trilha+'.html' for trilha in pastas] # Html de cada uma das trilhas declaradas na primeira linha
-
-# Pegar a ordem de cada seção dentro da trilha pelo html
-teste_html = html_trilhas[1]
-plain_html = open(teste_html, 'r', encoding='utf8').read()
-soup = BeautifulSoup(plain_html, 'html.parser')
-secoes_em_ordem = [t for t in soup.find_all(text=True) if t.parent.name in ['h4']]
-
-print(secoes_em_ordem)
-
-
-
-for pasta in pastas:
     # ENTRAR NO HTML DE CADA AULA -> VER A ORDEM DAS AULAS -> ADICIONAR AS AULAS EM ORDEM
+    pasta = pasta_trilhas + '/' + getSubfolderNameThatContains(pasta_trilhas, trilha)
     full_secoes = getFoldersFromDir(pasta)
-    #html_secoes = getHtmlFromDir(pasta)
     html_secoes = [secao+'.html' for secao in full_secoes]
+
+    # Pegar a ordem de cada seção dentro da trilha pelo html
+    trilha_html = pasta + '.html'                                                      # Html de cada da trilha
+    plain_html = open(trilha_html, 'r', encoding='utf8').read()                        # Lendo o html
+    soup = BeautifulSoup(plain_html, 'html.parser')                                    # HTML Parser
+    secoes_em_ordem = [t for t in soup.find_all(text=True) if t.parent.name in ['h4']] # As seções estão em um elemento h4
+
+    for secao in secoes_em_ordem:
+        insertSecaoInTrilha(secao, trilha, cursor, db)
+        print('kkk')
+        full_aulas = getFoldersFromDir(pasta)
+        html_aulas = [secao + '.html' for secao in full_secoes]
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import mysql.connector
 def executeScriptsFromFile(filename, cursor):
     # Open and read the file as a single buffer
     fd = open(filename, encoding="utf8")
@@ -33,4 +34,15 @@ def getFoldersFromDir(dir):
     dir = dir if dir != '' else None
     return [dir + '/' + d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
 
+def insertSecaoInTrilha(secao, trilha, cursor, db):
+    id = str(getTrilhaIdByName(trilha, cursor)[0][0])
+    cursor.execute('INSERT INTO secao (id_trilha, nome) VALUES (' + id + ', "' + secao + '")')
+    db.commit()
+
+def getTrilhaIdByName(name,cursor):
+    cursor.execute('SELECT id_trilha FROM trilha WHERE nome LIKE "%' + name + '%"')
+    return cursor.fetchall()
+
 #def orderDirByOrderArray(order_array, dir):
+
+#def clearIdentificationOfDir:
